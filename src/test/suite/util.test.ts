cb6709a52be598033886eab8d59eb23697b9b34a
Brian Njogu <brian@MacBook-Air.local>
2020-12-05 17:25:51 +0300

    BN: more tests

--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -1,5 +1,6 @@
 import { assert, expect } from 'chai';
-import {getNonce} from "../../util/";
+import {getNonce, getWebviewPanelTitle} from "../../util/";
+import { OpenedPdfFile } from "../../annotations/interfaces";
 
 suite('Utils Test Suite', () => {
 	test('nonce should return a string', () => {
@@ -10,5 +11,23 @@ suite('Utils Test Suite', () => {
 	test('nonce should be 32 characters in length', () => {
         let nonce: string = getNonce();
 		expect(nonce.length).to.be.equal(32);
+	});
+
+	test('panelTitle should be the name of the pdf', () => {
+		let file: OpenedPdfFile = {path: "/somePath/example.pdf"};
+		let panelTitle: string = getWebviewPanelTitle(file);
+		assert.equal(panelTitle, "example");
+	});
+
+	test('panelTitle should be \Soma\ if file is undefined', () => {
+		let file: undefined = undefined;
+		let panelTitle: string = getWebviewPanelTitle(file);
+		assert.equal(panelTitle, "Soma");
+	});
+
+	test('panelTitle should be \Soma\ if .pdf extension not in path', () => {
+		let file: OpenedPdfFile = {path: "/somePath/example"};
+		let panelTitle: string = getWebviewPanelTitle(file);
+		assert.equal(panelTitle, "Soma");
 	});
 });
